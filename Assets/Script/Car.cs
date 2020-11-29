bff169cd32a241dd89d5fb9892df51797ae031dd
Kaktys Koder <kaktyskoder@gmail.com>
2020-11-29 20:42:43 +0400

    :3

---
+++ Assets/Script/Car.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    [Tooltip("Экземпляр (префаб или ссылка) на Car")]
+    [SerializeField] private GameObject carPrefab;
+    [SerializeField] private float speedGresCar = 1.0f;
+
+    public CharacterController characterController;
+    public float speed = 12.0f;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Vector3 tempGresCarPosition = transform.position;
+
+        tempGresCarPosition.z += speedGresCar * Time.deltaTime;
+
+        transform.position = tempGresCarPosition;
+
+        float x = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * z + transform.forward * x;
+
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    transform.Rotate(0, -45, 0);
+
+        //    speedGresCar = -speedGresCar;
+
+        //}
+        //else if(Input.GetKeyDown(KeyCode.D))
+        //{
+        //    transform.Rotate(0, 45, 0);
+
+        //}
+
+
+        characterController.Move(move * speed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Script/TestAxis.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TestAxis : MonoBehaviour
+{
+    [SerializeField] private float speedMovement = 10.0f;
+    [SerializeField] private float rotationSpeed = 100.0f;
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Horizontal");
+
+
+
+        //transform.Translate(0, 0, translation);
+
+        //transform.Rotate(0, rotation, 0);
+    }
+}
